de54f0c64446af318175db21f80d9dab348d827d
ompatelz <omhareshpatel321@gmail.com>
2025-08-15 19:48:03 +0530

    Create starter project

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <h1>Welcome to Your Project!</h1>
+    <p class="subtitle">This is a simple starter template ready for your ideas.</p>
+    <div class="card">
+      <h2>✨ Ready to build something amazing?</h2>
+      <p>Start editing the files to see your changes live!</p>
+    </div>
+  </div>
+`
